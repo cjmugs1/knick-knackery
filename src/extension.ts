5841e990e5d449ffb05f7a0160d4e11dd7cc7ef8
connormulligan <connormulligan@gameplan.tech>
2026-03-11 14:48:36 -0400

    update mac os installer and add file watcher to extension

--- src/extension.ts
+++ src/extension.ts
@@ -2,13 +2,22 @@ import * as vscode from 'vscode';
 import { KnickKnackeryProvider } from './webview';
 import { saveTerminalContext, saveEditorSelection } from './saveTerminalSelection';
 import { setApiKey } from './setApiKey';
+import { KNICK_KNACKS_DIR } from './paths';
 
 export function activate(context: vscode.ExtensionContext) {
     const provider = new KnickKnackeryProvider(context.extensionUri);
-    
+
     context.subscriptions.push(
         vscode.window.registerWebviewViewProvider(KnickKnackeryProvider.viewType, provider)
     );
+
+    // Watch for knick-knacks created externally (e.g., via Hammerspoon hotkey)
+    const knickKnacksPattern = new vscode.RelativePattern(vscode.Uri.file(KNICK_KNACKS_DIR), '*.md');
+    const watcher = vscode.workspace.createFileSystemWatcher(knickKnacksPattern);
+    watcher.onDidCreate(() => provider.updateWebview());
+    watcher.onDidChange(() => provider.updateWebview());
+    watcher.onDidDelete(() => provider.updateWebview());
+    context.subscriptions.push(watcher);
 
     // Register the manual selection command
     let disposable1 = vscode.commands.registerCommand('knick-knackery.saveSelection', () => {
